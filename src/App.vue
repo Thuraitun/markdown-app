24a48e7c881ae6712101b140ff7ed0811a33aac2
Thuraitun <thuraitun64@gmail.com>
2023-07-10 01:56:37 +0630

    markdown commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <MarkDown />
+  </div>
+</template>
+
+<script>
+  import MarkDown from './components/MarkDown.vue';
+  export default {
+    name: 'App',
+    components: {
+      MarkDown,
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
